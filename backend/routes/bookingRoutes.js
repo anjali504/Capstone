7f1bba21ad6a9c5d4c92a65a3d60ae057d02fa00
anjali504 <104060040+anjali504@users.noreply.github.com>
2024-08-10 18:02:50 -0400

    frontend code

--- backend/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bookingController = require('../controllers/bookingController');
-
-router.post('/', bookingController.createBooking);
-
-router.get('/organizer/:organizerId', bookingController.getBookingsByOrganizer);
-
-module.exports = router;
